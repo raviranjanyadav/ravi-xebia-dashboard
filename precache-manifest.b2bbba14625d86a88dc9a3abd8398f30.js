b9082aecd2bf9404fa204fd8046277fb423edef5
BKB <raviyadavex@gmail.com>
2020-01-07 01:02:58 +0530

    Updates

---
+++ precache-manifest.b2bbba14625d86a88dc9a3abd8398f30.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb06d7690ef09490f7b95b1a43d9404a",
+    "url": "/ravi-xebia-dashboard/index.html"
+  },
+  {
+    "revision": "716968b781f0a3188db5",
+    "url": "/ravi-xebia-dashboard/static/css/main.9006e647.chunk.css"
+  },
+  {
+    "revision": "f0f0a4cbba6f0f3bb474",
+    "url": "/ravi-xebia-dashboard/static/js/2.d8542783.chunk.js"
+  },
+  {
+    "revision": "716968b781f0a3188db5",
+    "url": "/ravi-xebia-dashboard/static/js/main.bc7dadbf.chunk.js"
+  },
+  {
+    "revision": "92394a048f73bd16967f",
+    "url": "/ravi-xebia-dashboard/static/js/runtime~main.d29bbfb3.js"
+  }
+]);
